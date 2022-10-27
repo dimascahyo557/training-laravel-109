da1bdc50a5da98c83d83bc9a7f8173525dea40c4
dimascahyo557 <dimasamid@gmail.com>
2022-10-27 11:06:25 +0700

    day 4

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        // Query sales graph
+        $chart = $this->getChartData();
+
+        // Query latest transaction
+        // $transactions = Transaction::latest()->limit(10)->get();
+        $transactions = Transaction::orderBy('trx_date', 'desc')->limit(10)->get();
+
+        return view('admin.dashboard')
+            ->with('months', $chart['months'])
+            ->with('totals', $chart['totals'])
+            ->with('transactions', $transactions);
+    }
+
+    private function getChartData()
+    {
+        $sql = "SELECT LEFT(MONTHNAME(trx_date), 3) month, count(*) total FROM transactions "
+                . "WHERE YEAR(trx_date) = " . date('Y') . " "
+                . "GROUP BY month "
+                . "ORDER BY MONTH(trx_date)";
+
+        $transactions = DB::select($sql);
+
+        $months = [];
+        $totals = [];
+
+        foreach ($transactions as $transaction) {
+            $months[] = $transaction->month;
+            $totals[] = $transaction->total;
+        }
+        return [ 'months' => $months, 'totals' => $totals ];
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
                     <h3 class="card-title">Sales Graph</h3>
                 </div>
                 <div class="card-body">
-                    Sales graph here <br><br><br><br>
+                    <canvas class="chart" id="sales-chart" style="height: 250px"></canvas>
                 </div>
             </div>
         </div>
@@ -19,9 +19,50 @@
                     <h3 class="card-title">Latest Transaction</h3>
                 </div>
                 <div class="card-body">
-                    Latest transaction here <br><br><br><br>
+                    
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Date</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($transactions as $transaction)
+                                <tr>
+                                    <td>{{ $transaction->product->name }}</td>
+                                    <td>{{ $transaction->trx_date }}</td>
+                                    <td>{{ $transaction->price }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    var ctx = document.getElementById('sales-chart')
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: {!! json_encode($months) !!},
+            datasets: [
+                {
+                    label: 'Overall Sales',
+                    fill: true,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    borderColor: 'rgba(255, 99, 132)',
+                    data: {!! json_encode($totals) !!}
+                }
+            ]
+        }
+    })
+</script>
 @endsection
